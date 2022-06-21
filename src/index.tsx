e59d9ee08a6d454b1c6c3595cd9cefbf5d900e19
sweetpea22 <cindychau24@me.com>
2022-06-20 22:21:17 -0400

    Add elements wheee

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -12,6 +12,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useCallback } from 'react'
+import './index.scss';
+import * as networks from '../../networks.json';
+import { debounce } from '../../debounce';
+
+export const Home: React.FC = () => {
+  const [copied, setCopied] = useState(-1)
+  const debouncedSwitchCopied = debounce(() => setCopied(-1), 3000)
+
+  const onCopyInfo = useCallback((id: number, text: string) => {
+    navigator.clipboard.writeText(text)
+      .then(() => {
+        setCopied(id)
+        debouncedSwitchCopied()
+      })
+      .catch(console.error)
+
+
+    setCopied(id)
+    debouncedSwitchCopied()
+  }, [debouncedSwitchCopied])
+
+  return (
+    <div className='container'>
+      <aside className='container--aside'>
+        <h1>Multicall List</h1>
+        <p>A website that lists multicall contract addresses for various networks.</p>
+        <button><a href="/" rel="noopener noreferrer" target="_blank">View Code</a></button>
+      </aside>
+      <section className='container--networks'>
+        {Object.values(networks).map(({name, address}, index) => (
+          <div className='network' key={index}>
+            <div className='network--text-content'>
+              <h3>{name}</h3>
+              <button onClick={() => onCopyInfo(1, address)}>
+                {copied === 1 && (
+                  <span className='copied'>
+                    Copied!
+                  </span>
+                )}
+                {copied !== 1 && (
+                  <span className='copied'>
+                    Copy contract address
+                  </span>
+                )}
+              </button>
+            </div>
+          </div>
+        ))}
+      </section>
+    </div>
+  )
+}
